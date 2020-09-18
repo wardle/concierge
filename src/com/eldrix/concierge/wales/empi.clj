c335c90e4f6656031f7d8ae370e4242460d1faa9
Mark Wardle <mark@wardle.org>
2020-09-18 18:22:24 +0100

    Initial commit

---
+++ src/com/eldrix/concierge/wales/empi.clj
@@ -0,0 +1,38 @@
+(ns com.eldrix.concierge.wales.empi 
+  (:require 
+   [clojure.java.io :as io]
+   [clj-http.client :as client]
+   [selmer.parser]))
+
+(def endpoint-urls 
+  {:live "https://mpilivequeries.cymru.nhs.uk/PatientDemographicsQueryWS.asmx?wsdl"
+   :test "https://mpitest.cymru.nhs.uk/PatientDemographicsQueryWS.asmx?wsdl"
+   :dev "http://ndc06srvmpidev2.cymru.nhs.uk:23000/PatientDemographicsQueryWS.asmx?wsdl"})
+
+
+(comment
+
+  (:dev endpoint-urls)
+  (:live endpoint-urls)
+  (io/resource "wales-empi-request.xml")
+  (def req-body (slurp (io/resource "wales-empi-request.xml")))
+  req-body
+  
+  (def req (selmer.parser/render-file (io/resource "wales-empi-request.xml") 
+                      {:sending-application 221
+                       :sending-facility 221
+                       :receiving-application 100
+                       :receiving-facility 100
+                       :date-time "20200916020202"
+                       :message-control-id "uuid-to-be-made"
+                       :processing-id "T"   ;  P or U or T 
+                       :identifier "1234567890"
+                       :authority "NHS"  ;; empi organisation code
+                       :authority-type  "NH"
+                       }))
+  
+  (client/post (:dev endpoint-urls) {:content-type "text/xml; charset=\"utf-8\""
+                                     :headers {"SOAPAction" "http://apps.wales.nhs.uk/mpi/InvokePatientDemographicsQuery"}
+                                     :body req})
+
+  )
