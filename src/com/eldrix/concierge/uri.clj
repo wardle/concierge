3559bd42fd581c46d87b3fb6feb684938ff9edfa
Mark Wardle <mark@wardle.org>
2020-10-08 09:29:27 +0100

    Start a list (mainly for documentation) of well-known types.

--- src/com/eldrix/concierge/uri.clj
+++ src/com/eldrix/concierge/uri.clj
@@ -1,2 +1,17 @@
 (ns com.eldrix.concierge.uri)
 
+(def namespaces
+  "Well-known namespaces. This list is not exhaustive, and namespaces
+   not listed here can be used without formally registering their existence."
+  
+  {
+   ;;
+   ;; THESE ARE ALREADY DEFINED NAMESPACES EITHER INTERNATIONALLY OR NATIONALLY
+   ;; 
+   
+   
+   
+   ;;
+   ;; I HAVE MADE UP THESE PENDING FORMAL SCRUTINY / DISCUSSION / APPROVAL
+   ;; 
+   :wales.nhs.fhir/cymru-user-id "https://fhir.nhs.wales/Id/cymru-user-id"})
