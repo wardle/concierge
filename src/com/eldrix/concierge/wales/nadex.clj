e3423ffe94edb7408023e72aeddb065b6124ff50
Mark Wardle <mark@wardle.org>
2020-10-08 09:29:09 +0100

    Switch to using unboundid LDAP library.
    Permits use of SSL. This means that rather than using a generic account and an unencrypted channel to lookup user information, and using kerberos for credentials checks as a way of not transmitting user credentials in the clear, we use LDAPS bind to check credentials and to lookup user information.

--- src/com/eldrix/concierge/wales/nadex.clj
+++ src/com/eldrix/concierge/wales/nadex.clj
@@ -1,58 +1,87 @@
 (ns com.eldrix.concierge.wales.nadex
   "Integration with NHS Wales' active directory for authentication and user lookup."
-  (:require [clojure.java.io :as io])
+  (:require
+    [clojure.java.io :as io]
+    [com.eldrix.concierge.registry :as registry])
   (:import
-    (javax.naming Context)
-    (javax.naming.directory SearchControls InitialDirContext DirContext)
-    (java.util Map)
-    (com.eldrix.concierge.registry Resolver FreetextSearcher StructuredSearcher)))
+    (com.eldrix.concierge.registry Resolver FreetextSearcher StructuredSearcher)
+    (com.unboundid.ldap.sdk LDAPConnectionPool LDAPConnection LDAPBindException LDAPConnectionOptions
+                            SearchRequest SearchScope Filter Attribute)
+    (com.unboundid.util.ssl TrustAllTrustManager SSLUtil)
+    (java.util Collection)))
 
-(def base-ldap-env {Context/INITIAL_CONTEXT_FACTORY "com.sun.jndi.ldap.LdapCtxFactory"
-                    Context/PROVIDER_URL            "ldap://cymru.nhs.uk:389"
-                    Context/SECURITY_AUTHENTICATION "simple"})
+;; Unfortunately, the CYMRU domain uses a self-signed certificate. Alternatives would be to
+;; use a custom keystore or downgrade to using a non-encrypted channel of communication.
+;; In the circumstances, a man-in-the-middle attack within an intranet environment is unlikely.
+(defn make-unauthenticated-connection
+  "Creates a secure but unauthenticated connection, trusting all server certificates."
+  []
+  (LDAPConnection.
+    (.createSSLSocketFactory (SSLUtil. (TrustAllTrustManager.)))
+    (doto (LDAPConnectionOptions.)
+      (.setConnectTimeoutMillis 2000)
+      (.setFollowReferrals false))
+    "cymru.nhs.uk"
+    636))
 
-(defn- ^Map make-ldap-env [username password]
-  (merge base-ldap-env {Context/SECURITY_PRINCIPAL   username
-                        Context/SECURITY_CREDENTIALS password}))
+(def connection-pool-size 5)
+
+(defonce ^LDAPConnectionPool connection-pool
+         (delay (LDAPConnectionPool. (make-unauthenticated-connection) connection-pool-size)))
+
+(defn can-authenticate?
+  "Can the user 'username' authenticate using the 'password' specified?"
+  [username password]
+  (with-open [c (.getConnection @connection-pool)]
+    (try (.bind c (str username "@cymru.nhs.uk") password)
+         true
+         (catch LDAPBindException e false))))
 
 (def ^:private returning-attributes
   ["sAMAccountName" "displayNamePrintable"
    "personalTitle" "sn" "givenName"
-   "department" "mail", "title"                             ;; job title, not name prefix
+   "department" "mail", "title"                             ; title=job title, not name prefix
    "physicalDeliveryOfficeName"
    "postalAddress" "streetAddress" "l", "st", "postalCode"
    "telephoneNumber" "mobile" "company" "wWWHomePage" "postOfficeBox" "thumbnailPhoto"])
 
-(defn ^:private ^SearchControls search-controls [& params]
-  (doto (new SearchControls) (.setReturningAttributes (into-array String returning-attributes))
-                             (.setCountLimit (or (:count-limit params) 200))
-                             (.setTimeLimit (or (:time-limit params) 2000))
-                             (.setSearchScope SearchControls/SUBTREE_SCOPE)))
+(def ^:private binary-attributes
+  #{"thumbnailPhoto"})
 
-(defn parse-result [^javax.naming.directory.SearchResult result]
-  (let [attrs (enumeration-seq (.getAll (.getAttributes result)))]
-    (zipmap (map #(keyword (.getID %)) attrs)
-            (map #(let [v (enumeration-seq (.getAll %))] (if (= 1 (count v)) (first v) v)) attrs))))
+(defn parse-entry [^com.unboundid.ldap.sdk.SearchResultEntry result]
+  (let [attrs (.getAttributes result)]
+    (zipmap (map #(keyword (.getName %)) attrs)
+            (map #(let [v (.getValues ^Attribute %)] (if (= 1 (count v)) (first v) v)) attrs))))
 
-(defn search [username password ^String filter & params]
-  (let [^DirContext ldap-context (new InitialDirContext (java.util.Hashtable. (make-ldap-env username password)))
-        results (enumeration-seq (.search ldap-context "DC=cymru,DC=nhs,DC=uk" (str "(&(objectClass=User)" filter ")") (search-controls params)))]
-    (map parse-result results)))
+(defn by-username [^String username]
+  (Filter/createEqualityFilter "sAMAccountName" username))
 
-(defn make-filter
-  "Create a filter using the specified operator 'op' (\"&\" or \"|\")
-  and the specified params (a map of property and value)."
-  ([params] (make-filter "&" params))
-  ([op params]
-   (cond (string? params) (str "(" op params ")")
-         :else (let [clauses (map (fn [[k v]] (str "(" (name k) "=" v ")")) params)]
-                 (if (= 1 (count clauses)) (first clauses) (str "(" op (clojure.string/join clauses) ")"))))))
+(defn by-name [^String name]
+  (Filter/createANDFilter ^Collection
+                          (->> (clojure.string/split name #"\s+")
+                               (map #(Filter/createORFilter [(Filter/createSubInitialFilter "sn" ^String %)
+                                                             (Filter/createSubInitialFilter "givenName" ^String %)])))))
 
-(defn by-username [username]
-  (make-filter {:sAMAccountName username}))
+(defn by-params [params]
+  (let [clauses (map (fn [[k v]] (Filter/createEqualityFilter ^String (name k) ^String v)) params)]
+    (if (= 1 (count clauses))
+      (first clauses)
+      (Filter/createANDFilter ^Collection clauses))))
 
-(defn by-name [name]
-  (make-filter "&" (apply str (map #(make-filter "|" {:sn (str % "*") :givenName (str % "*")}) (clojure.string/split name #"\s+")))))
+(defn search
+  "Search for the user specified, either using their own credentials (and
+   implicitly searching for themselves, or using specific generic binding
+   credentials and the 'filter' specified."
+  ([username password] (search username password (by-username username)))
+  ([bind-username bind-password ^Filter search-filter]
+   (with-open [c (.getConnection @connection-pool)]
+     (.bind c (str bind-username "@cymru.nhs.uk") bind-password)
+     (let [results (.search c (SearchRequest.
+                                "DC=cymru,DC=nhs,DC=uk"
+                                SearchScope/SUB
+                                (Filter/createANDFilter [(Filter/createEqualityFilter "objectClass" "User") search-filter])
+                                (into-array String returning-attributes)))]
+       (map parse-entry (.getSearchEntries results))))))
 
 (deftype NadexService [username password]
   Resolver
@@ -60,7 +89,12 @@
     (search username password (by-username value)))
   StructuredSearcher
   (search-by-map [this system params]
-    (search username password (make-filter params)))
+    (search username password (by-params params)))
   FreetextSearcher
   (search-by-text [this system value]
     (search username password (by-name value))))
+
+(comment
+
+  )
+
