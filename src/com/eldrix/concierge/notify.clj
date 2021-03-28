02c9831908eec62adda3b4966f9dbb8a3893a886
Mark Wardle <mark@wardle.org>
2021-03-28 12:47:06 +0100

    Don't have dependency on any specific configuration system.
    Provide configuration in call instead.

--- src/com/eldrix/concierge/notify.clj
+++ src/com/eldrix/concierge/notify.clj
@@ -3,11 +3,7 @@
   (:require [clojure.string :as str]
             [clj-http.client :as client]
             [com.eldrix.concierge.config :as config]
-            [buddy.sign.jwt :as jwt]
-            [mount.core :as mount]))
-
-(defn api-key []
-  (:api-key (config/gov-uk-notify)))
+            [buddy.sign.jwt :as jwt]))
 
 (defn parse-api-key [k]
   (let [parts (str/split k #"-")]
@@ -28,28 +24,27 @@
 (defn notify!
   "Synchronously send a message using the `service` specified (:sms :email)
   using the parameters specified. Throws an exception if the request fails."
-  [service params]
+  [api-key service params]
   (client/post (str "https://api.notifications.service.gov.uk" (get services service))
-               {:headers               {"Authorization" (str "Bearer " (make-jwt (parse-api-key (api-key))))}
+               {:headers               {"Authorization" (str "Bearer " (make-jwt (parse-api-key api-key)))}
                 :content-type          :json
                 :form-params           params
                 :throw-entire-message? true}))
 
-(defn send-sms [phone-number template-id opts]
-  (notify! :sms {:phone_number    phone-number
-                 :template_id     template-id
-                 :personalisation opts}))
+(defn send-sms [api-key phone-number template-id opts]
+  (notify! api-key :sms {:phone_number    phone-number
+                         :template_id     template-id
+                         :personalisation opts}))
 
-(defn send-email [email template-id opts]
-  (notify! :email {:email_address   email
-                   :template_id     template-id
-                   :personalisation opts}))
+(defn send-email [api-key email template-id opts]
+  (notify! api-key :email {:email_address   email
+                           :template_id     template-id
+                           :personalisation opts}))
 
 (comment
-  (mount/start)
-  (mount/stop)
-  (api-key)
-
+  (require '[com.eldrix.concierge.config])
+  (def api-key (get-in (#'com.eldrix.concierge.config/config :dev) [:uk.gov/notify :api-key]))
+  api-key
   (def template-id "765ef309-74d0-43d8-80ea-1dbd9e92a3e8")
-  (send-sms "07786000000" template-id {:sender "System Administrator"})
+  (send-sms api-key "07786000000" template-id {:sender "System Administrator"})
   )
