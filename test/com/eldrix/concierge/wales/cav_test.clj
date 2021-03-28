99e821ee3b4136b3c6eba156a88e6fb317bba316
Mark Wardle <mark@wardle.org>
2021-03-28 17:38:23 +0100

    Add more complete CAV PMS testing.

--- test/com/eldrix/concierge/wales/cav_test.clj
+++ test/com/eldrix/concierge/wales/cav_test.clj
@@ -4,16 +4,31 @@
             [aero.core :as aero]
             [com.eldrix.concierge.wales.cav.pms :as pms]))
 
+(defn cav-config []
+  (:wales.nhs.cavuhb/pms (aero/read-config (io/resource "config.edn") {:profile :live})))
+
+(deftest ^:live test-auth-token
+  (is (pms/get-authentication-token (cav-config))))
+
+(deftest ^:live test-cav-fetch-crn
+  (let [config (cav-config)]
+    (is (nil? (pms/fetch-patient-by-crn config "A0")))
+    (is (pms/fetch-patient-by-crn config "a999998"))))
+
 (deftest ^:live test-cav-clinic-list
-  (let [config (aero/read-config (io/resource "config.edn") {:profile :live})
-        clinic-patients (pms/fetch-patients-for-clinics (:wales.nhs.cavuhb/pms config) ["neur58r" "neur58"] (java.time.LocalDate/of 2020 10 9))]
+  (let [config (cav-config)
+        clinic-patients (pms/fetch-patients-for-clinics config ["neur58r" "neur58"] (java.time.LocalDate/of 2020 10 9))]
     (clojure.pprint/print-table
      (->> clinic-patients
           (map #(select-keys % [:CONTACT_TYPE_DESC :START_TIME :END_TIME :HOSPITAL_ID :LAST_NAME :FIRST_FORENAME]))
           (sort-by :START_TIME)))
     (is (> (count clinic-patients) 0))))
 
+
 (comment
+  (println "Hello World")
+  (cav-config)
+  (pms/fetch-patient-by-crn (cav-config) "A999998")
   (run-tests))
 
 
