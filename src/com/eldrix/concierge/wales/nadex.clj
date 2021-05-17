cbcc29ebc5a444018b63b9ad8c1d73fbabe2dd78
Mark Wardle <mark@wardle.org>
2021-05-17 16:35:15 +0100

    Infer professional registration numbers.
    National directory appears to use an undocumented postOfficeBox field to store professional registration information for each user. This tries to safely parse this information, if it exists, and adds additional properties when available.

--- src/com/eldrix/concierge/wales/nadex.clj
+++ src/com/eldrix/concierge/wales/nadex.clj
@@ -62,6 +62,21 @@
             (let [v (.getValues attr)] (if (= 1 (count v)) (first v) v)))]
     [(keyword n) v]))
 
+(defn assoc-professional-registration-numbers
+  "Attempt to infer professional registration numbers from user data.
+  NHS Wales keeps the number in the postOfficeBox field of the national
+  directory. I am not sure this is documented anywhere, but this should fail
+  gracefully if this changes."
+  [{:keys [postOfficeBox] :as user}]
+  (let [[_ gmc-number] (re-matches #"^GMC:\s*(\d+)$" postOfficeBox)
+        [_ gphc-number] (re-matches #"^GPhC:\s*(\d+)$" postOfficeBox)]
+    (cond-> user
+            gmc-number
+            (assoc :urn.oid.2.16.840.1.113883.2.1.3.2.4.18.29 gmc-number
+                   :uk.org.hl7.fhir.Id/gmc-number gmc-number)
+            gphc-number
+            (assoc :uk.org.hl7.fhir.Id/gphc-number gphc-number))))   ;;https://fhir.hl7.org.uk/Id/gmc-number
+
 (defn parse-entry [^SearchResultEntry result]
   (into {} (map parse-attr (.getAttributes result))))
 
