24e75e17f315ab3c3764943a60922ae073b4afa1
Mark Wardle <mark@wardle.org>
2022-01-22 17:08:51 +0000

    Build using tools.build

---
+++ build.clj
@@ -0,0 +1,48 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'com.eldrix/concierge)
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+      (b/delete {:path "target"}))
+
+(defn jar [_]
+      (clean nil)
+      (println "Building   :" lib version)
+      (b/write-pom {:class-dir class-dir
+                    :lib lib
+                    :version version
+                    :basis basis
+                    :src-dirs ["src"]})
+      (b/copy-dir {:src-dirs ["src" "resources"]
+                   :target-dir class-dir})
+      (b/jar {:class-dir class-dir
+              :jar-file jar-file}))
+
+(defn install
+      "Installs pom and library jar in local maven repository"
+      [_]
+      (jar nil)
+      (println "Installing :" lib version)
+      (b/install {:basis basis
+                  :lib lib
+                  :class-dir class-dir
+                  :version version
+                  :jar-file jar-file}))
+
+
+(defn deploy
+      "Deploy library to clojars.
+      Environment variables CLOJARS_USERNAME and CLOJARS_PASSWORD must be set."
+      [_]
+      (install nil)
+      (println "Deploying  :" lib version)
+      (dd/deploy {:installer :remote
+                  :artifact  jar-file
+                  :pom-file  (b/pom-path {:lib       lib
+                                          :class-dir class-dir})}))
