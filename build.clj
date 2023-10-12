4133231d2c8aefff42fd479223cc9388d3ae6bc8
Mark Wardle <mark@wardle.org>
2023-10-12 11:01:42 +0100

    EPL v2.0

--- build.clj
+++ build.clj
@@ -28,15 +28,13 @@
                              [:developer
                               [:id "wardle"] [:name "Mark Wardle"] [:email "mark@wardle.org"] [:url "https://wardle.org"]]]
                             [:organization [:name "Eldrix Ltd"]]
-                            [:licenses
-                             [:license
-                              [:name "The Apache Software License, Version 2.0"]
-                              [:url "http://www.apache.org/licenses/LICENSE-2.0.txt"]
-                              [:distribution "repo"]]]]})
-  (b/copy-dir {:src-dirs   ["src" "resources"]
-               :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file  jar-file}))
+                            [:licenses [:license
+                                        [:name "Eclipse Public License v2.0"]
+                                        [:url "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html"]
+                                        [:distribution "repo"]]]]})
+  (b/copy-dir {:src-dirs ["src" "resources"], :target-dir class-dir})
+  (b/copy-file {:src "LICENSE" :target "target/classes/META-INF/LICENSE"})
+  (b/jar {:class-dir class-dir, :jar-file jar-file}))
 
 (defn install
   "Installs pom and library jar in local maven repository"
